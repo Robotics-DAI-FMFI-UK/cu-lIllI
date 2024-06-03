63764616c467a3b307c0b78ef7d9f4257396c511
Pavel Petrovič <petrovic16@uniba.sk>
2024-06-03 09:05:45 +0200

    fixing crc calculator

--- src/arduino/test_crc/test_crc.ino
+++ src/arduino/test_crc/test_crc.ino
@@ -4,17 +4,24 @@ void setup()
   Serial.print("CRC('hello')=");
   const char *msg = "hello";
   Serial.println(getCRC((uint8_t *)msg, strlen(msg), 0));
+  msg = "a bit longer string but not too much long anyway";
+  Serial.print("CRC(longer string)=");
+  Serial.println(getCRC((uint8_t *)msg, strlen(msg), 0));
+  msg = "a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know a quite longer string much much longer you know ";
+  Serial.print("CRC(long)=");
+  Serial.println(getCRC((uint8_t *)msg, strlen(msg), 0));
 }
 
 void loop() 
 {
 }
 
-uint8_t getCRC(uint8_t message[], uint8_t length, uint8_t previous_crc)
+uint8_t getCRC(uint8_t message[], uint32_t length, uint8_t previous_crc)
 {
-  static const uint8_t CRC7_POLY = 0x91;
-  uint8_t i, j, crc = previous_crc;
-
+  static const unsigned char CRC7_POLY = 0x91;
+  uint32_t i;
+  uint8_t j, crc = previous_crc;
+ 
   for (i = 0; i < length; i++)
   {
     crc ^= message[i];
@@ -22,7 +29,7 @@ uint8_t getCRC(uint8_t message[], uint8_t length, uint8_t previous_crc)
     {
       if (crc & 1)
         crc ^= CRC7_POLY;
-      crc >>= 1;      
+      crc >>= 1;
     }
   }
   return crc;
